4928d4eaa3e77a4f6cbb8bf9338942bddeaf1f9f
mikhail <michael.maidan1998@gmail.com>
2026-05-04 15:37:05 +0200

    feat: add CI/CD integration config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/rs-react-2026/',
   plugins: [react(), tailwindcss()],
 });
